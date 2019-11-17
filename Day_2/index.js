27c86ce2d65ec76e39c44134ef6ebfe742b64c52
KuYalda <o.igorok@gmail.com>
2019-11-17 12:33:05 +0200

    Classwork

---
+++ Day_2/index.js
@@ -0,0 +1,42 @@
+`use strict`;
+
+// const a = 20;
+
+// let result = a > 10 && a < 30;
+
+// console.log(result);
+
+// const b = 20;
+
+// let result = b < 10 || b > 10;
+
+// console.log(result);
+
+// const a = 10;
+// if (a === 10) {
+// alert("hello World!!!");
+// } else if (a = 10) {
+
+// const num = 15;
+
+// num < 10 ? console.log("Hello WOeld!!!") : console.log ("prepare");
+
+//  const month "November";
+
+// switch (key) {
+//      case value:
+         
+//          break;
+ 
+//      default:
+//          break;
+//  }
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
